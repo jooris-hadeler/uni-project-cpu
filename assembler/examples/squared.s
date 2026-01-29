7bb706047b5b4300dbadc3e01a6dcaca57b859c5
Jooris Hadeler <j.hadeler@outlook.com>
2026-01-29 16:19:33 +0100

    feat: Test

--- assembler/examples/squared.s
+++ assembler/examples/squared.s
@@ -1,11 +1,44 @@
 entry:
     mov $1, 1
+    mov $8, 40
 
 loop:
     nop
     nop
     nop
     nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
     add $1, $1, $1
 
-    jmp loop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    jr $8
